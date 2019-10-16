68ca0ea471c1c2c57dd392c11dd80551a977f596
lukaszinglot <luk.inglot@gmail.com>
2019-10-16 19:26:50 +0200

    comment delete

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,9 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
-// import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
-// const composedEnhancers = composeWithDevTools();
 const composeEnhancers =
   window["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"] || compose;
 
